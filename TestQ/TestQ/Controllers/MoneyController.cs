dffabc77fc7524736bc383e43ce66bbd31fa78c3
Cody Smith <codys33333@gmail.com>
2015-09-29 22:37:19 -0600

    final commit

--- TestQ/TestQ/Controllers/MoneyController.cs
+++ TestQ/TestQ/Controllers/MoneyController.cs
@@ -18,70 +18,65 @@ namespace TestQ.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(double amount)
+        public ActionResult Index(decimal amount = 0)
         {
-            ViewBag.intResult = String.Format("${0:0,0.00}", amount);
-             decimal fraction = (decimal)amount;
-             long iPart = (long)fraction;
-             int decimalResult = Convert.ToInt16((fraction % 1.0m) * 100);
-              
-                ViewBag.result = convertIntToWord(amount) + " and " +  decimalResult + "/100 dollars";
-
-           
-
+            if (amount != 0)
+            {
+                ViewBag.intResult = String.Format("${0:0,0.00}", amount);
+                int decimalResult = Convert.ToInt16((amount % 1.0m) * 100);
+                ViewBag.result = char.ToUpper(convertIntToWord(amount)[0]) + convertIntToWord(amount).Substring(1) + " and " + decimalResult + "/100 dollars";
+            }
             return View();
         }
-        public static string convertIntToWord(double amount)
+        public static string convertIntToWord(decimal amount)
         {
-            var n = (int)amount;
-            
-             
-
+            var n = (long)amount;
             if (n > 1000000000)
             {
-                return convertIntToWord(n / 1000000000) + "Billion " + convertIntToWord(n % 1000000000);
+                return convertIntToWord(n / 1000000000) + " billion " + convertIntToWord(n % 1000000000);
             }
             else if (n >= 1000000000 && n <= 1999999999)
             {
-                return "One Billion " + convertIntToWord(n % 1000000000);
+                return "one billion " + convertIntToWord(n % 1000000000);
             }
             else if (n >= 1000000 && n <= 999999999)
             {
-                return convertIntToWord(n / 1000000) + " Million " + convertIntToWord(n % 1000000);
+                return convertIntToWord(n / 1000000) + " million " + convertIntToWord(n % 1000000);
             }
             else if (n >= 1000000 && n <= 1999999)
             {
-                return "One Million " + convertIntToWord(n % 1000000);
+                return "one million " + convertIntToWord(n % 1000000);
             }
             else if (n >= 2000 && n <= 999999)
             {
-                return convertIntToWord(n / 1000) + " Thousand " + convertIntToWord(n % 1000);
+                return convertIntToWord(n / 1000) + " thousand " + convertIntToWord(n % 1000);
             }
             else if (n >= 1000 && n <= 1999)
             {
-                return "One Thousand " + convertIntToWord(n % 1000);
+                return "one Thousand " + convertIntToWord(n % 1000);
             }
             else if (n >= 200 && n <= 999)
             {
-                return convertIntToWord(n / 100) + " Hundred " + convertIntToWord(n % 100);
+                return convertIntToWord(n / 100) + " hundred " + convertIntToWord(n % 100);
             }
             else if (n >= 100 && n <= 199)
             {
-                return "One Hundred " + convertIntToWord(n % 100);
+                return "one Hundred " + convertIntToWord(n % 100);
             }
             else if (n >= 20 && n <= 99)
             {
-                var arr = new string[] { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+                var arr = new string[] { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
                 return arr[n / 10 - 2] + " " + convertIntToWord(n % 10);
             }
             else if (n > 0 && n <= 19)
             {
-                var arr = new string[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+                var arr = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
                 return arr[n - 1];
             }
           
             else
-                return "Zero";
-        }
+                return "";
+            }
+        
     }
 }
